5edbd7e10f03bef2353e0cea79d1b96145d5ce52
UJWAL PANDEY <63130636+ujwalpandey1@users.noreply.github.com>
2022-10-22 23:30:43 +0530

    Time: 22 ms (66.74%), Space: 6.1 MB (10.54%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    
+    
+    bool isPalindrome(int x) {
+        if(x<0) return false;
+        if(x==0) return true;
+        int power=floor(log10(x)+1);
+        power--;
+        
+        int num=x;
+        long long int num2=0;
+        while(num>0){
+            int digit=num%10;
+            num=num/10;
+            num2=num2+(digit*pow(10,power));
+            power--;
+        }
+       
+        return x==num2;
+    }
+};
