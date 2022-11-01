e03ec92e2eb24ee93eb95750fc69a6f32918520e
UJWAL PANDEY <63130636+ujwalpandey1@users.noreply.github.com>
2022-11-01 23:34:37 +0530

    Time: 222 ms (78.73%), Space: 71.4 MB (56.68%) - LeetHub

---
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    
+    bool check(vector<int>&nums,long long mid){
+        long long int carry=0;
+        for(int i=nums.size()-1;i>=0;i--){
+          carry+=nums[i];
+          if(carry-mid>0) carry=carry-mid;
+            else carry=0;
+        }
+        return carry==0;
+    }
+    int minimizeArrayValue(vector<int>& nums) {
+        int ans=0;
+        int l=0;
+        int r=1e9;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(check(nums,(long long)mid)){
+                ans=mid;
+                r=mid-1;
+            }
+            else l=mid+1;
+        }
+        return ans;
+    }
+};
