753f8037d920faeca4fe3483186b6a67f1de2032
UJWAL PANDEY <63130636+ujwalpandey1@users.noreply.github.com>
2022-11-03 13:10:50 +0530

    Time: 408 ms (69.13%), Space: 99 MB (86.19%) - LeetHub

---
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    
+    
+    int maxFrequency(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+       int i=0;
+        int j=0;
+        int n=nums.size();
+       long long  int sum=0;
+        int ans=0;
+        while(j<n){
+            sum+=nums[j];
+            if((long)(j-i+1)*(long)nums[j]-sum<=k){
+                ans=max(ans,j-i+1);
+               
+            }
+            else if((long)(j-i+1)*(long)nums[j]-sum>k){
+                while((long)(j-i+1)*(long)nums[j]-sum>k){
+                    sum-=nums[i];
+                  
+                    i++;
+                     if(i==j) break;
+                }
+            }
+            j++;
+        }
+        return ans;
+    }
+};
